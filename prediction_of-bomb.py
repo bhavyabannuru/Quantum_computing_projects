892afd585ce39a8221edc4f9c14c096202433880
bhavyabannuru <bhavyabannuru@gmail.com>
2026-02-07 21:05:41 +0530

    Updated prediction_of-bomb code

--- prediction_of-bomb.py
+++ prediction_of-bomb.py
@@ -1,37 +1,19 @@
-from qiskit import QuantumCircuit, transpile
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
 from qiskit_aer import AerSimulator
 import numpy as np
-
 N = int(input("enter no.of times you want to run the loop: "))
 theta = np.pi/(2*N)
-shots = 5000
+shots = 5
+q = QuantumRegister(1)
+c = ClassicalRegister(N + 1)
+qc = QuantumCircuit(q, c)
+for i in range(N):
+    qc.ry(2*theta, 0)
+    qc.measure(0, c[i])
+    qc.reset(0)
+
+qc.measure(0, c[N])
 sim = AerSimulator()
-
-# DUD bomb
-qc_dud = QuantumCircuit(1, 1)
-for _ in range(N):
-    qc_dud.ry(2*theta, 0)
-
-qc_dud.measure(0, 0)
-
-tqc = transpile(qc_dud, sim)
-result_dud = sim.run(tqc, shots=shots).result()
-
-print("\nDUD bomb counts:")
-print(result_dud.get_counts())
-
-
-# WORKING bomb
-qc_work = QuantumCircuit(1, 1)
-for _ in range(N):
-    qc_work.ry(2*theta, 0)
-    qc_work.measure(0, 0)
-    qc_work.reset(0)
-
-qc_work.measure(0, 0)
-
-tqcw = transpile(qc_work, sim)
-result_work = sim.run(tqcw, shots=shots).result()
-
-print("\nWORKING bomb counts:")
-print(result_work.get_counts())
+tqc = transpile(qc, sim)
+result = sim.run(tqc, shots=shots).result()
+print(result.get_counts())
