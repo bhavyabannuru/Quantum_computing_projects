5b0d8ad247b4640ede3a1fef4871c5c05d32c4a4
bhavyabannuru <bhavyabannuru@gmail.com>
2026-02-07 21:28:44 +0530

    quantum project

--- quantum_teliportation.py
+++ quantum_teliportation.py
@@ -1,19 +1,17 @@
 from qiskit import QuantumCircuit
-from qiskit.quantum_info import Statevector
-from qiskit.visualization import plot_bloch_vector
+import numpy as np
 import matplotlib.pyplot as plt
-#3 qubits,2 classical bits
-qc = QuantumCircuit(3)
-qc.h(0)        
+qc = QuantumCircuit(3, 2)
+qc.ry(np.pi/3, 0)
 qc.h(1)
 qc.cx(1, 2)
-#Alice's operations
 qc.cx(0, 1)
 qc.h(0)
-# Measure Alice's qubits
-qc.cx(1, 2)
-qc.cz(0, 2)
+qc.measure(0, 0)
+qc.measure(1, 1)
+with qc.if_test((1, 1)):
+    qc.x(2)
+with qc.if_test((0, 1)):
+    qc.z(2)
 qc.draw("mpl")
 plt.show()
-state = Statevector.from_instruction(qc)
-print("\nFinal Statevector:\n", state)
